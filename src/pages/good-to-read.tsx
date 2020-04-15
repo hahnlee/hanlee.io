76018aa6f42ba63347afd97ce378fe8e519cd03f
hahnlee <hanlee.dev@gmail.com>
2020-04-15 17:05:21 +0900

    Fix link

--- src/pages/good-to-read.tsx
+++ src/pages/good-to-read.tsx
@@ -7,63 +7,75 @@ function GoodToRead() {
     <Page>
       <ul>
         <li>
-        스티브의 구글 플랫폼 폭언
-        http://egloos.zum.com/eggry/v/3763434
+          <a href="http://egloos.zum.com/eggry/v/3763434">
+            스티브의 구글 플랫폼 폭언
+          </a>
         </li>
 
         <li>
-        넷플릭스의 문화 : 자유와 책임 (한국어 번역본)
-        https://www.slideshare.net/watchncompass/freedom-responsibility-culture/
+          <a href="https://www.slideshare.net/watchncompass/freedom-responsibility-culture/">
+            넷플릭스의 문화: 자유와 책임 (한국어 번역본)
+          </a>
         </li>
 
         <li>
-        Jeff Bezos, Jack Ma, and the Quest to Kill eBay
-        https://medium.com/s/story/jeff-bezos-jack-ma-and-the-quest-to-kill-ebay-bb4992dc5020
+          <a href="https://medium.com/s/story/jeff-bezos-jack-ma-and-the-quest-to-kill-ebay-bb4992dc5020">
+            Jeff Bezos, Jack Ma, and the Quest to Kill eBay
+          </a>
         </li>
 
         <li>
-        10배 뛰어난 개발자 되기
-        https://muchtrans.com/translations/10xdeveloper.ko.html
+          <a href="https://muchtrans.com/translations/10xdeveloper.ko.html">
+            10배 뛰어난 개발자 되기
+          </a>
         </li>
 
         <li>
-          Zen of Python
-          https://www.python.org/dev/peps/pep-0020/
+          <a href="https://www.python.org/dev/peps/pep-0020/">
+            Zen of Python
+          </a>
         </li>
 
         <li>
-        구조적 동시성에 대한 소고, 또는 Go 문의 해로움
-        https://muchtrans.com/translations/notes-on-structured-concurrency-or-go-statement-considered-harmful.ko.html
+          <a href="https://muchtrans.com/translations/notes-on-structured-concurrency-or-go-statement-considered-harmful.ko.html">
+            구조적 동시성에 대한 소고, 또는 Go 문의 해로움
+          </a>
         </li>
 
         <li>
-        Web API 디자인
-        https://docs.microsoft.com/ko-kr/azure/architecture/best-practices/api-design
+          <a href="https://docs.microsoft.com/ko-kr/azure/architecture/best-practices/api-design">
+            Web API 디자인
+          </a>
         </li>
 
         <li>
-          실리콘벨리의 팀장들
-          https://books.google.co.kr/books?id=NOadDwAAQBAJ
+          <a href="https://books.google.co.kr/books?id=NOadDwAAQBAJ">
+            실리콘벨리의 팀장들
+          </a>
         </li>
 
         <li>
-        클린코드
-        https://books.google.co.kr/books?id=MKsYngEACAAJ
+          <a href="https://books.google.co.kr/books?id=MKsYngEACAAJ">
+            클린코드
+          </a>
         </li>
 
         <li>
-        Let's Program Like It's 1999
-        https://www.youtube.com/watch?v=vG8WpLr6y_U
+          <a href="https://www.youtube.com/watch?v=vG8WpLr6y_U">
+            Let's Program Like It's 1999
+          </a>
         </li>
 
         <li>
-        React: CSS in JS – NationJS
-        http://blog.vjeux.com/2014/javascript/react-css-in-js-nationjs.html
+          <a href="http://blog.vjeux.com/2014/javascript/react-css-in-js-nationjs.html">
+            React: CSS in JS – NationJS
+          </a>
         </li>
 
         <li>
-        Bug-O 노테이션
-        https://overreacted.io/ko/the-bug-o-notation/
+          <a href="https://overreacted.io/ko/the-bug-o-notation/">
+            Bug-O 노테이션
+          </a>
         </li>
       </ul>
     </Page>
